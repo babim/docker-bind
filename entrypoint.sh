9a5c7cbbff7948f57b62cde6c01eeb8ea3a25d37
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-27 15:32:40 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+# value
+FORWARD1=${FORWARD1:-8.8.8.8}
+FORWARD2=${FORWARD1:-8.8.4.4}
+$DOMAIN=${DOMAIN:-example.lan}
+NS1${NS1:-192.168.0.1}
+MAIL${MAIL:-192.168.0.1}
+POP3${POP3:-192.168.0.1}
+IMAP${IMAP:-192.168.0.1}
+IMAP4${IMAP4:-192.168.0.1}
+SMTP${SMTP${:-192.168.0.1}
+
+# set config
+if [ ! -f "/etc/bind/named.conf" ]; then
+cat <<EOF>> /etc/bind/named.conf
+options {
+	directory "/var/bind";
+	forwarders {
+		$FORWARD1;
+		$FORWARD2;
+	};
+
+	listen-on port 53 { any; };
+	listen-on-v6 { none; };
+	pid-file "/var/run/named/named.pid";
+	allow-transfer { none; };
+	allow-query     { any; };
+	recursion yes;
+	dnssec-enable yes;
+    	dnssec-validation yes;
+    	dnssec-lookaside auto;
+};
+
+zone "." IN {
+	type hint;
+	file "named.ca";
+};
+
+zone "localhost" IN {
+	type master;
+	file "pri/localhost.zone";
+	allow-update { none; };
+	notify no;
+};
+
+zone "127.in-addr.arpa" IN {
+	type master;
+	file "pri/127.zone";
+	allow-update { none; };
+	notify no;
+  
+zone "$DOMAIN" {
+        type master;
+        file "/etc/bind/db.$DOMAIN";
+};
+EOF
+touch /etc/bind/db.$DOMAIN
+cat <<EOF >/etc/bind/db.$DOMAIN
+\$TTL  604800
+@      IN      SOA    ns1.$DOMAIN. root.localhost. (
+                              2        ; Serial
+                        604800        ; Refresh
+                          86400        ; Retry
+                        2419200        ; Expire
+                        604800 )      ; Negative Cache TTL
+;
+@     IN      NS      ns1.$DOMAIN.
+@     IN      A      $CONTAINERIP
+@     IN      MX     10     $HOSTNAME.$DOMAIN.
+$HOSTNAME     IN      A      $CONTAINERIP
+ns1     IN      A      $NS1
+mail     IN      A      $MAIL
+pop3     IN      A      $POP3
+imap     IN      A      $IMAP
+imap4     IN      A      $IMAP4
+smtp     IN      A      $SMTP
+EOF
+fi
+
+exec "$@"
